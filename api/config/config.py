b28cbeaa9d6d06467f79f457de622e4fc12dd669
Lusengeri <lusengeri@gmail.com>
2022-08-22 20:24:41 +0300

    load flask config values from environment variables

--- api/config/config.py
+++ api/config/config.py
@@ -7,17 +7,17 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@db:5432/flask_demo_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True 
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_STRING') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/test_db"
-    SQLALCHEMY_ECHO = False 
+    SQLALCHEMY_ECHO = False
 
     JWT_SECRET_KEY="b'\x99\xceK\x0ev\x9b\xb6\xf7q\x89`\x92\xcf]"
     SECRET_KEY = 'my_secured_key_here'
